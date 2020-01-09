40448e7546b112919b816839afca72868349e0e5
Sunitha Bussa <sbussa@dciinc.org>
2020-01-09 17:18:27 -0600

    more sidebar development done

--- DCI_midcourse/global.R
+++ DCI_midcourse/global.R
@@ -3,4 +3,19 @@ library(tidyverse)
 library(DT)
 library(dplyr)
 library(ggplot2)
- 
+
+
+dci_data <- readRDS('data/dci_data_shiny.rds')
+
+
+year<-dci_data %>%
+  pull(dos_year) %>%
+  unique()
+
+month<-dci_data %>%
+  pull(dos_month) %>%
+  unique()
+
+
+modality <- c('HIC','PD','HH')
+
--- DCI_midcourse/ui.R
+++ DCI_midcourse/ui.R
@@ -11,7 +11,7 @@ library(shiny)
 library(shinythemes)
  
 dbHeader <- dashboardHeader()
-dbHeader$children[[3]]$children <- tags$h3(tags$img(src='kidney-bean.png',height='60',width='100'),"Welcome to Dialysis Clinic Incorporation!",align = "middle")
+dbHeader$children[[3]]$children <- tags$h2(tags$img(src='kidney-bean.png',height='60',width='100'),"Welcome to Dialysis Clinic Incorporation",align = "middle")
 dbHeader$children[[2]]$children <-  tags$a(href='http://www.dciinc.org',
                                            tags$img(src='DCI_logo.png',height='40',width='190'))
 
@@ -22,12 +22,15 @@ shinyUI(
    
     dbHeader,
     dashboardSidebar(
-      sidebarMenu(
+      sidebarMenu(id = "sidebarmenu",
         menuItem("Introduction",tabName = "introduction", icon = icon("user-md",class= NULL, lib="font-awesome")),
-          menuSubItem("Dashboard Finance", tabName = "finance"),
-          menuSubItem("Dashboard Sales", tabName = "sales"),
-        menuItem("Detailed Analysis"),
-        menuItem("Raw Data")
+          menuSubItem("Summary", tabName = "summary"),
+          conditionalPanel("input.sidebarmenu=='summary'",
+                           sliderInput("summary","pick year",2016,2020,1)),
+          menuSubItem("Detail", tabName = "detail"),
+        menuItem("Modalities",tabName = "modality"),
+        menuItem("Features",tabName = "feature"),
+        menuItem("Raw Data",tabName = "data")
       )
     ),
     dashboardBody(
@@ -37,10 +40,18 @@ shinyUI(
                 fluidRow(
                   box(h2("first tab"))
                 )),
-        tabItem(tabName = "finance",
-                h1("Finance Dashboard")),
-        tabItem(tabName = "sales",
-                h2("Sales Dashboard"))
+        tabItem(tabName = "summary",
+                h1("Summary Analysis"),
+                h2("try")
+                ),
+        tabItem(tabName = "detail",
+                h1("Detail Analysis")),
+        tabItem(tabName = "modality",
+                h1("Modality Analysis")),
+        tabItem(tabName = "feature",
+                h1("Features Analysis")),
+        tabItem(tabName = "data",
+                h1("Raw Data"))
       )
     )
     
